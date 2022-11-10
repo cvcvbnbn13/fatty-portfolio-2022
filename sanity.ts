c038275fffed4671aab5039c3b786bde48ce0ca7
邵謙(Shau Chien) <cvcvbnbn13@gmail.com>
2022-11-10 16:45:00 +0800

    [feat]Add-new-env-sanity-token

--- sanity.ts
+++ sanity.ts
@@ -6,7 +6,7 @@ export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: '2021-10-21',
-  useCdn: process.env.NODE_ENV === 'production',
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 }
 
 // set up the client for fetching data in the getProps page functions
