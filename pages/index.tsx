69121360189709862284019b446fc2f5de381e6a
邵謙(Shau Chien) <cvcvbnbn13@gmail.com>
2022-11-10 18:05:33 +0800

    [fix]

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,7 @@ const Home: NextPage<IProps> = ({
   projects,
   socials,
 }) => {
-  console.log(1)
+  console.log(2)
 
   return (
     <div className="bg-[#1c2f2f] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]">
